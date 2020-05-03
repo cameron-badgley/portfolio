efba50eb25bca38b9c1ef910e7a36b4f8c78dfa5
cameron-badgley <cameron@logicamsoftware.com>
2020-05-03 15:00:43 -0400

    Added Project Euler 001

---
+++ katas/project-euler/project-euler-001.py
@@ -0,0 +1,30 @@
+ #!/bin/python3
+import sys
+import math
+
+t = int(input().strip())
+for a0 in range(t):
+    n = int(input().strip())
+
+    # Iterating for n and adding up the multiples would take O(n).
+    # Instead, we can calculate the finite arithmetic 
+    # series in O(1) using: 
+    # 
+    # 1/2 * n * (a_1 + a_n) 
+    # 
+    # Where:
+    # n = the number of terms being added
+    # a_1 = the first element in the sequence
+    # a_n = the last element in the sequence
+    
+    multiplesOfThree = (n - 1) // 3
+    sumOfMultiplesOfThree = 3 * (multiplesOfThree * (multiplesOfThree + 1)) // 2
+
+    multiplesOfFive = (n - 1) // 5
+    sumOfMultiplesOfFive = 5 * (multiplesOfFive * (multiplesOfFive + 1)) // 2
+
+    # Find the multiples that were counted twice (for both 3 and 5)
+    multiplesOfFifteen = (n - 1) // 15
+    sumOfMultiplesOfFifteen = 15 * (multiplesOfFifteen * (multiplesOfFifteen + 1)) // 2
+
+    print(int(sumOfMultiplesOfThree + sumOfMultiplesOfFive - sumOfMultiplesOfFifteen))
