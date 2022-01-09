9fca84394d250964a063ca9893c9172c130d76e3
cameron-badgley <cameron@logicamsoftware.com>
2022-01-09 16:33:11 -0500

    Converted product list to use http observable.

--- projects/angular/Angular-GettingStarted-master/APM/src/app/products/product-list-component.ts
+++ projects/angular/Angular-GettingStarted-master/APM/src/app/products/product-list-component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
 import { IProduct } from "./product";
 import { ProductService } from "./product.service";
 
@@ -7,11 +8,13 @@ import { ProductService } from "./product.service";
     templateUrl: './product-list.component.html',
     styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit {
-    pageTitle = 'Product List';
-    imageWidth = 50;
-    imageMargin = 2;
-    showImage = false;
+export class ProductListComponent implements OnInit, OnDestroy {
+    pageTitle : String = 'Product List';
+    imageWidth : number = 50;
+    imageMargin : number = 2;
+    showImage : boolean = false;
+    errorMessage : string = '';
+    sub!: Subscription; 
 
     private _listFilter: string = '';
 
@@ -44,8 +47,17 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        this.products = this.productService.getProducts();
-        this.filteredProducts = this.products;
+        this.sub = this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMessage = err
+        });
+    }
+
+    ngOnDestroy(): void {
+        this.sub.unsubscribe();
     }
 
     onRatingClicked(message: string): void{
